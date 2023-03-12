c20b2bb27eeb9f9d28b85052802c634c8693fe68
Datafox-JCP <datafox@outlook.com>
2023-03-12 16:07:57 -0600

    Initial Commit

---
+++ ChatGPT-Toolbar/ChatGPT_ToolbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPT_ToolbarApp.swift
+//  ChatGPT-Toolbar
+//
+//  Created by Juan Hernandez Pazos on 12/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPT_ToolbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
