1f75db9350080e478846eb980cc6e5481e18b021
Datafox-JCP <datafox@outlook.com>
2023-03-13 07:09:36 -0600

    Initial sample

--- ChatGPT-Toolbar/ContentView.swift
+++ ChatGPT-Toolbar/ContentView.swift
@@ -6,19 +6,65 @@
 //
 
 import SwiftUI
+import OpenAISwift
+
+// Package: https://github.com/adamrushy/OpenAISwift
 
 struct ContentView: View {
+    // MARK: Properties
+    @State private var search: String = ""
+    @State private var responses: [String] = []
+    
+    // Get api key: https://platform.openai.com
+    let openAI = OpenAISwift(authToken: "")
+    
+    private var isFormValid: Bool {
+        !search.isEmpty
+        // no permitir espacios
+    }
+    
+    // MARK: Functions
+    private func performSearch() {
+        responses.append("You: \(search)")
+        
+        openAI.sendCompletion(with: search, maxTokens: 500) { result in
+            switch result {
+            case .success(let success):
+                let response = "ChatGPT: \(success.choices.first?.text ?? "")"
+                responses.append(response)
+            case .failure(let failure):
+                print(failure.localizedDescription)
+            }
+        }
+    }
+    
+    // MARK: View
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+            HStack {
+                TextField("Search...", text: $search)
+                    .textFieldStyle(.roundedBorder)
+                
+                Button {
+                    performSearch()
+                } label: {
+                    Image(systemName: "magnifyingglass.circle")
+                        .font(.title)
+                }
+                .buttonStyle(.borderless)
+                .disabled(!isFormValid)
+            } // HStack
+            
+            List(responses, id: \.self) { response in
+                Text(response)
+            } // List
+            .listStyle(.plain)
+        } // VStack
         .padding()
     }
 }
 
+// MARK: Preview
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
